b9df7cc22caaa06678c086f1951fb885b11064e7
sheriff Oladimeji <dimejiademola5@gmail.com>
2023-05-07 11:16:00 +0100

    feat: add nav to desktop

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,17 @@
 import { logo } from "../assets"
-
+import { navLinks } from "./data"
 const Navbar = () => {
   return (
       <header>
-          <nav className="hidden md:flex  w-[80px] mx-auto py-8">
-              <div>
+          <nav className="hidden md:flex  w-[80px] mx-auto py-8 items-center">
+              <div className="flex items-center">
                   <img src={logo} alt="" />
-                  <ul>
-                      
+                  <ul className="flex space-x-4 ml-4 text-GrayishViolet">
+                      {navLinks.map((link,) => (
+                          <li key={link.id}>
+                              <a href={`#${link.content}`}>{ link.content}</a>  
+                          </li>
+                      ))}  
                   </ul>
               </div>
               <div></div>
--- src/components/data.js
+++ src/components/data.js
@@ -12,3 +12,5 @@ const navLinks = [
     content: "Resources",
   },
 ];
+const 
+export{navLinks}
