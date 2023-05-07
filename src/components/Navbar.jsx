d14ab598988a7d363d6bfe32fbdf271e77de15a4
sheriff Oladimeji <dimejiademola5@gmail.com>
2023-05-07 10:27:33 +0100

    modify files

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import { logo } from "../assets"
+
+const Navbar = () => {
+  return (
+      <header>
+          <nav>
+              <div>
+                  <img src={logo} alt="" />
+              </div>
+              <div></div>
+          </nav>
+      </header>
+  )
+}
+
+export default Navbar
