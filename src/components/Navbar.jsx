d516988869e60581c65a11c4ea92bb686de49e03
sheriff Oladimeji <dimejiademola5@gmail.com>
2023-05-07 11:38:41 +0100

    fix : fix margin of nav links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
       <nav className="hidden md:flex justify-between w-[80%] mx-auto py-8">
         <div className="flex items-center">
           <img src={logo} alt="" />
-          <ul className="flex space-x-4 ml-4 text-GrayishViolet font-semibold">
+          <ul className="flex space-x-6 ml-8 text-GrayishViolet font-semibold">
             {navLinks.map((link) => (
               <li key={link.id}>
                 <a href={`#${link.content}`}>{link.content}</a>
@@ -15,7 +15,7 @@ const Navbar = () => {
           </ul>
         </div>
 
-        <ul className="flex items-center space-x-4 font-semibold">
+        <ul className="flex items-center space-x-6 font-semibold">
           {CTA.map((link) => (
             <li key={link.id} className={`${link.id === 2?" bg-Cyan rounded-full py-1 w-[100px] text-[#FFf] text-center ":"text-GrayishViolet"}`}>
               <a href="">{link.content}</a>
