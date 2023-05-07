c9a30cd9a56c534e6c99146262086693cc3f29e9
sheriff Oladimeji <dimejiademola5@gmail.com>
2023-05-08 00:03:24 +0100

    modify files

--- src/components/Shorten.jsx
+++ src/components/Shorten.jsx
@@ -1,19 +1,18 @@
-import Button from "./Button"
-
+import Button from "./Button";
 
 const Shorten = () => {
   return (
-    <section className="w-[90%] md:w-[80%] mx-auto bg-DarkViolet py-8 my-8 rounded-md">
-      <form className="w-[90%] flex flex-col md:flex-row mx-auto gap-8">
+    <section className=" w-[90%] md:w-[80%] mx-auto bg-DarkViolet py-8 my-8 rounded-md">
+      <form className="w-[90%] flex flex-col md:flex-row mx-auto gap-4 md:gap-8">
         <input
           type="text"
-          className="p-4  border w-full md:w-3/4 rounded-md"
+          className="p-4  border-none w-full md:w-3/4 rounded-md outline-none placeholder:font-semibold placeholder:text-GrayishViolet"
           placeholder="Shorten a link here..."
         />
-        <Button style={`py-4 md:w-1/4 rounded-md`} value={"Shorten It!"} />
+        <Button style={`py-4 md:w-1/4 rounded-md font-bold`} value={"Shorten It!"} />
       </form>
     </section>
   );
-}
+};
 
-export default Shorten
+export default Shorten;
