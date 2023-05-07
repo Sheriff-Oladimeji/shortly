9ecfc953df58a5333bc8bc660f94bd0cc4a98aa4
sheriff Oladimeji <dimejiademola5@gmail.com>
2023-05-07 05:31:49 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+    colors: {
+      Cyan: "hsl(180, 66%, 49%)",
+      DarkViolet: "hsl(257, 27%, 26%)",
+      Gray: "hsl(0, 0%, 75%)",
+      GrayishViolet: "hsl(257, 7%, 63%)",
+      VeryDarkBlue: "hsl(255, 11%, 22%)",
+      VeryDarkViolet: "hsl(260, 8%, 14%)",
+      Red: "hsl(0, 87%, 67%)",
+    },
+    fontFamily: {
+      Poppins: ["Poppins", "sans-serif"],
+    },
+  
+  },
+  plugins: [],
+};
