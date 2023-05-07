5770a4579e0c0aa19b6196329e968ce926113a60
sheriff Oladimeji <dimejiademola5@gmail.com>
2023-05-07 15:38:32 +0100

    modify files

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,8 +5,8 @@ import {SlMenu} from "react-icons/sl"
 const Navbar = () => {
   const [toggle , setToggle]  = useState(false)
   return (
-    <header className="w-full font-Poppins text-[16px]">
-      <nav className="hidden md:flex justify-between w-[80%] mx-auto py-8">
+    <header className="w-full font-Poppins ">
+      <nav className="hidden md:flex justify-between w-[80%] mx-auto py-8 text-[16px]">
         <div className="flex items-center">
           <img src={logo} alt="logo" />
           <ul className="flex space-x-6 ml-8 text-GrayishViolet font-semibold">
@@ -34,7 +34,7 @@ const Navbar = () => {
         </ul>
       </nav>
 
-      <nav className="md:hidden relative h-[200px]">
+      <nav className="md:hidden relative h-[200px] text-[18px]">
         <div className="flex items-center justify-between py-8 w-[90%] mx-auto">
           <img src={logo} alt="logo" />
           <button
@@ -47,7 +47,7 @@ const Navbar = () => {
         <ul
           className={`${
             toggle === true ? "scale-100 " : "scale-0"
-          } w-[90%] mx-auto my-1  bg-DarkViolet text-[#fff]  rounded-xl space-y-6 flex flex-col items-center py-8 z-50 transform duration-300 relative h-[400px]`}
+          } w-[90%] mx-auto my-1  bg-DarkViolet text-[#fff]  rounded-xl space-y-6 flex flex-col items-center py-8 z-50 transform duration-300 relative h-[400px] justify-between font-bold`}
         >
           {navLinks.map((link) => (
             <li key={link.id}>
@@ -55,7 +55,14 @@ const Navbar = () => {
             </li>
           ))}
           {CTA.map((link) => (
-            <li key={link.id}>
+            <li
+              key={link.id}
+              className={`${
+                link.id === 2
+                  ? " bg-Cyan rounded-full py-2 w-[90%] text-[#FFf] text-center "
+                  : ""
+              }`}
+            >
               <a href={`#${link.content}`}>{link.content}</a>
             </li>
           ))}
