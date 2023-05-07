348149bb2af652d5f2d5189ffabe433ab099776f
sheriff Oladimeji <dimejiademola5@gmail.com>
2023-05-07 14:43:49 +0100

    feat: add usestate to tracck toggle btn

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,14 @@
+import { useState } from "react";
 import { logo } from "../assets";
 import { navLinks, CTA } from "./data";
+import {SlMenu} from "react-icons/sl"
 const Navbar = () => {
+  const [toggle , setToggle]  = useState(false)
   return (
-    <header className="w-full font-Poppins">
+    <header className="w-full font-Poppins text-[16px]">
       <nav className="hidden md:flex justify-between w-[80%] mx-auto py-8">
         <div className="flex items-center">
-          <img src={logo} alt="" />
+          <img src={logo} alt="logo" />
           <ul className="flex space-x-6 ml-8 text-GrayishViolet font-semibold">
             {navLinks.map((link) => (
               <li key={link.id}>
@@ -17,8 +20,36 @@ const Navbar = () => {
 
         <ul className="flex items-center space-x-6 font-semibold">
           {CTA.map((link) => (
-            <li key={link.id} className={`${link.id === 2?" bg-Cyan rounded-full py-1 w-[100px] text-[#FFf] text-center ":"text-GrayishViolet"}`}>
+            <li
+              key={link.id}
+              className={`${
+                link.id === 2
+                  ? " bg-Cyan rounded-full py-2 w-[100px] text-[#FFf] text-center "
+                  : "text-GrayishViolet"
+              }`}
+            >
               <a href="">{link.content}</a>
+            </li>
+          ))}
+        </ul>
+      </nav>
+
+      <nav className="md:hidden">
+        <div className="flex items-center justify-between py-8 w-[90%] mx-auto">
+          <img src={logo} alt="logo" />
+          <button className="bg-none text-GrayishViolet cursor-pointer font-bold">
+            <SlMenu size={30} />
+          </button>
+        </div>
+        <ul className="w-[90%] mx-auto my-1 py-4 bg-DarkViolet text-[#fff]  rounded-xl space-y-6 flex flex-col items-center py-8">
+          {navLinks.map((link) => (
+            <li key={link.id}>
+              <a href={`#${link.content}`}>{link.content}</a>
+            </li>
+          ))}
+          {CTA.map((link) => (
+            <li key={link.id}>
+              <a href={`#${link.content}`}>{link.content}</a>
             </li>
           ))}
         </ul>
