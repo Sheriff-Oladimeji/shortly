bce4be85df383168855d80bc545095490cc2a612
sheriff Oladimeji <dimejiademola5@gmail.com>
2023-05-10 05:46:23 +0100

    feat: add submit function to form

--- src/components/Shorten.jsx
+++ src/components/Shorten.jsx
@@ -1,10 +1,13 @@
 import Button from "./Button";
 
 const Shorten = () => {
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+  }
   return (
     <section className="shorten-bg w-[90%] md:w-[80%] mx-auto bg-DarkViolet py-8 my-8 rounded-md  z-30" >
       <div></div>
-      <form className="w-[90%] flex flex-col md:flex-row mx-auto gap-4 md:gap-8">
+      <form className="w-[90%] flex flex-col md:flex-row mx-auto gap-4 md:gap-8" onSubmit={handleSubmit}>
         <input
           type="text"
           className="p-4  border-none w-full md:w-3/4 rounded-md outline-none placeholder:font-semibold placeholder:text-GrayishViolet"
@@ -13,6 +16,7 @@ const Shorten = () => {
         <Button
           style={`py-4 md:w-1/4 rounded-md font-bold`}
           value={"Shorten It!"}
+          type="submit"
         />
       </form>
     </section>
