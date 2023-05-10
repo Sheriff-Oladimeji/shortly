9e68de937486979a0d8fede13bd05a2ad6c3fb13
sheriff Oladimeji <dimejiademola5@gmail.com>
2023-05-10 17:53:50 +0100

    fix: fix card display

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,12 +2,16 @@
 
 const Card = ({icon, title, content}) => {
   return (
-      <div className="bg-[#fff] w-[100%] relative">
-          <img src={icon} alt="icon" className="absolute" />
-          <h4>{title}</h4>
-          <p>{content}</p>
-   </div>
-  )
+    <div className="bg-[#fff] w-[100%] relative">
+      <div className="absolute  transform -translate-x-1/2 -translate-y-1/2 bg-DarkViolet rounded-full p-6 md:left-[20%] left-1/2">
+        <img src={icon} alt="icon" />
+      </div>
+      <div className="p-8 mt-10  text-center md:text-start">
+        <h4 className="font-bold text-DarkViolet">{title}</h4>
+        <p className="max-w-xl text-GrayishViolet">{content}</p>
+      </div>
+    </div>
+  );
 }
 
 export default Card
