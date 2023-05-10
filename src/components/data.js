7f71fcc93afd2596d7d141e481fb519b5c165132
sheriff Oladimeji <dimejiademola5@gmail.com>
2023-05-10 08:08:28 +0100

    feat: add card data to data.jsx

--- src/components/data.js
+++ src/components/data.js
@@ -1,3 +1,4 @@
+import { brandIcon, recordIcon, customizeIcon } from "../assets";
 const navLinks = [
   {
     id: 1,
@@ -22,4 +23,25 @@ const CTA = [
     content: "Sign Up",
   },
 ];
-export{navLinks, CTA}
+
+const cards = [
+  {
+    id: 1,
+    icon: brandIcon,
+    title: "Brand Recognition",
+    content: ` Boost your brand recognition with each click. Generic links don’t mean a thing. Branded links help instil confidence in your content.`,
+  },
+  {
+    id: 2,
+    icon: recordIcon,
+    title: "Detailed Records",
+    content: `Gain insights into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions.`,
+  },
+  {
+    id: 3,
+    icon: customizeIcon,
+    title: "Fully Customizable",
+    content: ` Improve brand awareness and content discoverability through customizable links, supercharging audience engagement.`,
+  },
+];
+export { navLinks, CTA, cards };
