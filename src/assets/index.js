41a6aaa9f7881178ffd1924d9627ce1f7305a621
sheriff Oladimeji <dimejiademola5@gmail.com>
2023-05-10 05:51:07 +0100

    import card Icons from assets

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,3 +1,6 @@
 import logo from "./logo.svg"
 import working from "./illustration-working.svg";
-export{logo, working}
+import brandIcon from "./icon-brand-recognition.svg";
+import recordIcon from "./icon-detailed-records.svg";
+import customizeIcon from "./icon-fully-customizable.svg";
+export{logo, working, brandIcon, customizeIcon, recordIcon}
